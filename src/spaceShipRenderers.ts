54851ffed5b96ec285311a909918624450eee468
John Edvard <johnedvard89@gmail.com>
2021-08-31 16:14:42 +0900

    chore: Remove space ship renderer (#4) (#5)
    test optional test

--- src/spaceShipRenderers.ts
+++ src/spaceShipRenderers.ts
@@ -39,31 +39,9 @@ export const renderCoolDefaultSpaceShip = (
   }
 };
 
-export const renderSpikySpaceShip = (sprite: Sprite) => {
-  sprite.context.fillStyle = sprite.color;
-  sprite.context.strokeStyle = sprite.color;
-  sprite.context.lineWidth = 4;
-  sprite.context.beginPath();
-  sprite.context.lineCap = 'round';
-  sprite.context.moveTo(0, 0); // top left corner
-  sprite.context.arc(
-    sprite.height - 1,
-    sprite.width / 3,
-    sprite.height - 12,
-    0,
-    90
-  );
-  sprite.context.moveTo(0, 0); // top left corner
-  sprite.context.lineTo(sprite.width, sprite.height / 2); // bottom
-  sprite.context.lineTo(0, sprite.height); // top right corner
-  sprite.context.lineTo(0, 0); // close path
-  sprite.context.fill();
-};
-
 export const spaceShipRenderers: any[] = [
   renderDefaultSpaceShip,
   renderCoolDefaultSpaceShip,
-  renderSpikySpaceShip,
   (sprite: Sprite) => renderDefaultSpaceShip(sprite, true),
   (sprite: Sprite) => renderCoolDefaultSpaceShip(sprite, true),
 ];
